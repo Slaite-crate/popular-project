64b19f8ca9b7f0655359ccdab050f9926287e8f8
Pelle <pelle.park@gmail.com>
2020-12-20 09:51:47 +0100

    hello

--- src/main/java/dk/kea/popular/controller/VideoController.java
+++ src/main/java/dk/kea/popular/controller/VideoController.java
@@ -1,15 +1,11 @@
 package dk.kea.popular.controller;
 
-import dk.kea.popular.model.Video;
 import dk.kea.popular.repository.VideoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class VideoController {
@@ -22,41 +18,37 @@ public class VideoController {
         return "index";
     }
 
-    @GetMapping("/videos")
+    @GetMapping("/video")
     public String readAll(Model model){
         model.addAttribute("videos", videoRepository.findAll());
-        return "videos";
+        return "video";
     }
 
-    @GetMapping("/videosbyname")
-    public String readAllByName(Model model){
-        ArrayList<Video> vl = new ArrayList<>();
-        for (Video v : videoRepository.findAll()){
-            vl.add(v);
+    @GetMapping("/video/{by}")
+    public String readAllBy(@PathVariable String by, Model model){
+        switch (by){
+            case "name":
+                model.addAttribute("videos", videoRepository.findAllByOrderByName());
+                break;
+            case "url":
+                model.addAttribute("videos", videoRepository.findAllByOrderByUrl());
+                break;
+            case "views":
+                model.addAttribute("videos", videoRepository.findAllByOrderByViewsDesc());
+                break;
+            case "likes":
+                model.addAttribute("videos", videoRepository.findAllByOrderByLikesDesc());
+                break;
+            case "length":
+                model.addAttribute("videos", videoRepository.findAllByOrderByLengthDesc());
+                break;
+            case "date":
+                model.addAttribute("videos", videoRepository.findAllByOrderByDateDesc());
+                break;
+            case "category":
+                model.addAttribute("videos", videoRepository.findAllByOrderByCategory());
+                break;
         }
-        Collections.sort(vl, new Comparator<Video>(){
-            @Override
-            public int compare(Video v1, Video v2){
-                return v1.getName().compareToIgnoreCase(v2.getName());
-            }
-        });
-        model.addAttribute("videos", vl);
-        return "videos";
-    }
-
-    @GetMapping("/videosbyviews")
-    public String readAllByViews(Model model){
-        ArrayList<Video> vl = new ArrayList<>();
-        for (Video v : videoRepository.findAll()){
-            vl.add(v);
-        }
-        Collections.sort(vl, new Comparator<Video>() {
-            @Override
-            public int compare(Video o1, Video o2) {
-                return o1.getViews() - o2.getViews();
-            }
-        });
-        model.addAttribute("videos", vl);
-        return "videos";
+        return "video";
     }
 }
--- src/main/java/dk/kea/popular/repository/VideoRepository.java
+++ src/main/java/dk/kea/popular/repository/VideoRepository.java
@@ -4,4 +4,11 @@ import dk.kea.popular.model.Video;
 import org.springframework.data.repository.CrudRepository;
 
 public interface VideoRepository extends CrudRepository<Video, Long> {
+    public Iterable<Video> findAllByOrderByName();
+    public Iterable<Video> findAllByOrderByUrl();
+    public Iterable<Video> findAllByOrderByViewsDesc();
+    public Iterable<Video> findAllByOrderByLikesDesc();
+    public Iterable<Video> findAllByOrderByLengthDesc();
+    public Iterable<Video> findAllByOrderByDateDesc();
+    public Iterable<Video> findAllByOrderByCategory();
 }
